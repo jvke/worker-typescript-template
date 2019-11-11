6092ca8f88d0a18e9f5e2b944e320ee35b7c2b66
jvke <hi@jvke.co>
2019-11-11 21:19:59 +1100

    chore(deps): switch to yarn

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { handleRequest } from './handler'
 
-interface IEvent extends Event {
-  respondWith: (K: Promise<Response>) => Promise<Response>
-  request: Request
-}
-
 addEventListener('fetch', event => {
-  ;(event as IEvent).respondWith(handleRequest((event as IEvent).request))
+  event.respondWith(handleRequest(event.request))
 })
